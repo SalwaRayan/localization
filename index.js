d7852240bb1b4f7e23bac20928b44287c3652562
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-12-08 10:43:52 +0100

    done

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ const engine = require('express-handlebars').engine
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
+app.use(express.static("public"));
+
 const translation = require('./translation.json')
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`)
-})
 
 app.get('/:lang?', (req, res) => {
   const { lang } = req.params
-  const langue = "fr"
+  const langue = lang
 
   res.render('home', {
+    langue,
     pageTitle: translation[langue].pageTitle,
     title: translation[langue].title,
   })
 })
 
+app.get('*', (req, res) => {
+  res.send('page not found')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
